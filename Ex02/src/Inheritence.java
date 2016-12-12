088585a2e731a2d8e007bf78cb78394c2b80eec0
Your Name <you@example.com>
2016-12-12 20:49:29 +0900

    ...

---
+++ Ex02/src/Inheritence.java
@@ -0,0 +1,36 @@
+
+public class Inheritence {
+	public static void main(String[] args) {
+	class ParentClass{
+		private int x, y;
+		public ParentClass(){
+			System.out.println("parent class constructor");
+		}
+		public ParentClass(int x, int y){
+			super();
+			this.x = x;
+			this.y = y;
+		}
+		public void printxy(){
+			System.out.println("parent x ="+ x+"y =" +y);
+		}
+	}
+	class ChildClass extends ParentClass {
+		private int x,y;
+		
+		public ChildClass(){
+			System.out.println("child class constructor");
+		}
+		public ChildClass(int x, int y){
+			super(x*2, y*3);
+			this.x = x;
+			this.y = y;
+		}
+		public void printxy(){
+			System.out.println("child x ="+ x+"y =" +y);
+		}
+	}
+	ChildClass child = new ChildClass(3, 4);
+	child.printxy();
+	}
+}
